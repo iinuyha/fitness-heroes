322405bfada1059d1bcbf85c2bd0e73fa34c1367
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-10-24 15:48:02 +0900

    코인 + 정보표시 컴포넌트로 빼냄

--- front/src/pages/menu/page.jsx
+++ front/src/pages/menu/page.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { routes } from "../../constants/routes";
-import Popup from "../../components/Popup"; // Popup 컴포넌트 불러오기
+import CoinInfoDisplay from "../../components/CoinInfoDisplay"; // InfoPopup 컴포넌트 불러오기
 
 function MenuPage() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -15,26 +15,8 @@ function MenuPage() {
       className="relative w-full h-screen bg-cover bg-center"
       style={{ backgroundImage: "url('/image/background.png')" }}
     >
-      {/* 좌측 상단 코인 이미지 및 숫자 */}
-      <div className="absolute top-4 left-4 flex items-center">
-        <img src="/image/menu/coin_icon.png" alt="Coin" className="w-10 h-10" />
-        <p className="text-white text-2xl ml-2">9</p>
-      </div>
-
-      {/* 우측 상단 i 아이콘 */}
-      <div className="absolute top-4 right-4">
-        <button onClick={handlePopupOpen}>
-          <img src="/image/info_icon.png" alt="Info" className="w-8 h-8" />
-        </button>
-      </div>
-
-      {/* 팝업창 */}
-      {isPopupOpen && (
-        <Popup
-          message="이 게임에 대한 정보입니다."
-          onClose={handlePopupOpen} // 닫기 핸들러 전달
-        />
-      )}
+      {/* 코인 및 정보 팝업 */}
+      <CoinInfoDisplay message="# 이 게임에 대한 정보입니다.  <u>dd</u> **운동을 통해 에너지를 모아보세요!**" />
 
       {/* 가운데 5개 아이콘 메뉴 */}
       <div className="flex flex-col items-center justify-center h-full space-y-8 font-sans">
