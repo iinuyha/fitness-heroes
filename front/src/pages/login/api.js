f6ff4266a019855c552d38a63ccac264666e8f06
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-11-04 09:41:53 +0900

    로그인 관련 api 수정

--- front/src/pages/login/api.js
+++ front/src/pages/login/api.js
@@ -6,7 +6,15 @@ const api = axios.create({
 
 export const login = async (id, password) => {
   try {
-    const response = await api.post("/api/login", { id, password });
+    const response = await api.post(
+      "/api/login",
+      { id, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     return response.data;
   } catch (error) {
     throw error.response ? error.response.data : new Error("Login failed");
