24ae60f68748993506661c07433fa1cc47599300
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-11-04 09:51:28 +0900

    코인 관련 api 및 로직 작성

---
+++ front/src/components/api/coinAPI.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL,
+});
+
+export const getCoinCount = async () => {
+  try {
+    const token = localStorage.getItem("jwtToken");
+    const response = await api.get("/api/user/coin", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data.coin;
+  } catch (error) {
+    console.error("Failed to fetch coin count:", error);
+    throw error;
+  }
+};
