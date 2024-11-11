3109528139310db6ff6937fafa699cabd96f0561
Son Ahyun <dkgus731@naver.com>
2024-11-11 11:25:41 +0900

    스토리 페이지 끝

--- back/routes/exerciseRouter.js
+++ back/routes/exerciseRouter.js
@@ -1,13 +1,13 @@
-// 운동 정보 조회
 const express = require("express");
 const router = express.Router();
+const Exercise = require("../models/exercise"); // Exercise 모델 불러오기
 
 // GET /api/exercise 엔드포인트
 router.get("/", async (req, res) => {
   try {
-    const { concern, gender } = req.body;
+    const { concern, gender } = req.query;
 
-    // 데이터베이스에서 조건에 맞는 모든 운동 정보 조회
+    // 데이터베이스에서 운동 정보 조회
     const exerciseInfo = await Exercise.find({ concern, gender });
 
     if (!exerciseInfo || exerciseInfo.length === 0) {
--- front/src/pages/story/episode/api.js
+++ front/src/pages/story/episode/api.js
@@ -1,28 +1,14 @@
 import axios from "axios";
 
-export const getStoryEpisode = async (token) => {
-  try {
-    const response = await axios.get(
-      `${process.env.REACT_APP_SERVER_URL}/api/story`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    return response.data; // 사용자가 수행한 에피소드 정보 목록 [{ concern: string, episode: number, date: string }]
-  } catch (error) {
-    console.error("Failed to fetch story episode:", error);
-    throw error;
-  }
-};
-
-export const getEpCardData = async () => {
+export const getEpCardData = async (concern, gender) => {
   try {
     const response = await axios.get(
       `${process.env.REACT_APP_SERVER_URL}/api/exercise`,
       {
+        params: {
+          concern,
+          gender,
+        },
         headers: {
           "Content-Type": "application/json",
         },
