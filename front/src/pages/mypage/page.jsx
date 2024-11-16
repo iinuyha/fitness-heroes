4e2c739a474ff490c540d9acc2a557975f81976e
Son Ahyun <dkgus731@naver.com>
2024-11-16 09:28:45 +0900

    로그아웃 시, 소켓 연결 해제

--- front/src/pages/mypage/page.jsx
+++ front/src/pages/mypage/page.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { jwtDecode } from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 import { routes } from "../../constants/routes";
@@ -6,32 +6,34 @@ import Popup from "../../components/Popup";
 import CoinInfoDisplay from "../../components/CoinInfoDisplay";
 import ReturnDisplay from "../../components/ReturnDisplay";
 import { getUserInfo, getStoryInfo } from "./api";
+import SocketContext from "../../contexts/SocketContext";
 
 function MypagePage() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [popupMessage, setPopupMessage] = useState("");
   const [currentIndex, setCurrentIndex] = useState(0);
   const [userInfo, setUserInfo] = useState({});
-  const [storyInfo, setStoryInfo] = useState([]); // 초기 상태를 빈 배열로 설정
+  const [storyInfo, setStoryInfo] = useState([]);
 
-  const navigate = useNavigate(); // useNavigate 훅을 사용하여 페이지 이동
+  const { socket } = useContext(SocketContext); // SocketContext에서 socket을 가져옵니다.
+  const navigate = useNavigate();
 
-  // 정보 불러오기
   useEffect(() => {
     const fetchUserInfo = async () => {
       try {
-        const token = localStorage.getItem("token"); // 토큰 가져오기
+        const token = localStorage.getItem("token");
         const data = await getUserInfo(token);
         setUserInfo(data);
       } catch (error) {
         handlePopupOpen("사용자 정보를 불러오는데 실패했습니다.");
       }
     };
+
     const fetchStoryInfo = async () => {
       try {
-        const token = localStorage.getItem("token"); // 토큰 가져오기
+        const token = localStorage.getItem("token");
         const data = await getStoryInfo(token);
-        setStoryInfo(data.slice(1)); // 첫 번째 에피소드는 0으로 기본 설정이 되어있으니까 첫 번째 원소를 제외한 나머지만!!
+        setStoryInfo(data.slice(1)); // 첫 번째 원소를 제외
       } catch (error) {
         handlePopupOpen("스토리 정보를 불러오는데 실패했습니다.");
       }
@@ -39,7 +41,7 @@ function MypagePage() {
 
     fetchUserInfo();
     fetchStoryInfo();
-  }, []); // 의존성 배열 추가하여 한 번만 실행되도록 설정
+  }, []);
 
   const handlePopupOpen = (message) => {
     setPopupMessage(message);
@@ -47,15 +49,16 @@ function MypagePage() {
   };
 
   const handleLogout = () => {
+    if (socket) {
+      socket.disconnect(); // 소켓 연결 종료
+    }
     localStorage.removeItem("token"); // 토큰 삭제
     navigate(routes.main); // 홈 페이지로 이동
   };
 
-  // 페이지당 4개씩 표시
   const itemsPerPage = 4;
   const totalPages = Math.ceil(storyInfo.length / itemsPerPage);
 
-  // 슬라이드 이동 핸들러
   const nextSlide = () => {
     if (currentIndex < totalPages - 1) {
       setCurrentIndex(currentIndex + 1);
@@ -68,7 +71,6 @@ function MypagePage() {
     }
   };
 
-  // 현재 페이지에 표시할 에피소드
   const currentStoryInfo = storyInfo.slice(
     currentIndex * itemsPerPage,
     currentIndex * itemsPerPage + itemsPerPage
@@ -79,13 +81,9 @@ function MypagePage() {
       className="relative w-full h-screen bg-cover bg-center"
       style={{ backgroundImage: "url('/image/background.png')" }}
     >
-      {/* 뒤로가기 버튼 */}
       <ReturnDisplay />
-
-      {/* 코인 및 정보 팝업 */}
       <CoinInfoDisplay message="마이페이지 관련된 설명 적으면 됨" />
 
-      {/* 팝업창 */}
       {isPopupOpen && (
         <Popup message={popupMessage} onClose={() => setIsPopupOpen(false)} />
       )}
@@ -98,7 +96,6 @@ function MypagePage() {
           마이페이지
         </h1>
 
-        {/* 사용자 정보 */}
         <div className="flex bg-white bg-opacity-20 rounded-xl p-10 text-white w-2/4 justify-center">
           <div className="flex items-center justify-between">
             <div className="font-semibold">
@@ -129,7 +126,6 @@ function MypagePage() {
           </div>
         </div>
 
-        {/* 에피소드 슬라이드 */}
         <div className="bg-white bg-opacity-20 rounded-xl p-10 text-white w-2/4 relative">
           <div className="mb-4 flex justify-between">
             <h2 className="text-xl font-semibold text-white">지난 에피소드</h2>
@@ -138,7 +134,6 @@ function MypagePage() {
             </div>
           </div>
 
-          {/* 에피소드 카드 그룹 */}
           <div className="flex items-center justify-center relative">
             {currentIndex > 0 && (
               <button
@@ -151,7 +146,7 @@ function MypagePage() {
             <div className="flex space-x-4 w-full max-w-2xl mx-10 justify-center">
               {currentStoryInfo.map((episode) => (
                 <div
-                  key={`${episode.concern}-${episode.episode}`} // concern과 episode를 결합하여 key로 설정
+                  key={`${episode.concern}-${episode.episode}`}
                   className="bg-white rounded-lg p-4 w-1/4 text-center text-gray-800"
                 >
                   <h3 className="font-semibold text-lg">
@@ -176,7 +171,6 @@ function MypagePage() {
             )}
           </div>
 
-          {/* 인디케이터 */}
           <div className="flex justify-center mt-4 space-x-2">
             {Array.from({ length: totalPages }, (_, index) => (
               <span
@@ -189,7 +183,6 @@ function MypagePage() {
           </div>
         </div>
 
-        {/* 로그아웃 버튼 */}
         <button
           onClick={handleLogout}
           className="mt-1 px-4 py-2 bg-red-500 text-white rounded-lg font-semibold"
