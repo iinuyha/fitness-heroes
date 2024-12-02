491d7b7c77cc5fff203d05b9eb5462a33c86bd95
Haemin Kim <haemin1230@gmail.com>
2024-12-03 03:18:15 +0900

    challenge status declinde로 변경 api

--- back/routes/matchRouter.js
+++ back/routes/matchRouter.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const router = express.Router();
 const Friend = require("../models/friend");
 const Character = require("../models/character");
+const Challenge = require("../models/challenge");
 
 const secretKey = "hi"; // 인증에 사용할 비밀 키
 
@@ -162,6 +163,50 @@ router.post("/reduce-coin", authenticateToken, async (req, res) => {
     console.error("코인 차감 오류:", error);
     res.status(500).json({ error: "코인 차감 실패" });
   }
+});
+
+// Challenge status를 declined로 변경
+router.post("/status-declined", async (req, res) => {
+  const { roomId } = req.body;
+
+  try {
+    // roomId에서 챌린저와 챌린지드 ID 추출
+    const [challengerId, challengedId] = roomId.split("-");
+
+   // 가장 최근의 Challenge 데이터 가져오기
+   const matchData = await Challenge.findOne(
+    { challengerId: challengerId, challengedId: challengedId } 
+    ).sort({ createdAt: -1 });
+
+
+    if (!matchData) {
+      return res.status(404).json({
+        status: "error",
+        message: "해당 데이터를 찾을 수 없습니다.",
+      });
+    }
+
+    matchData.status = "declined";
+    await matchData.save();
+    
+
+    // 성공 응답 반환
+    return res.status(200).json({
+      status: "success",
+      message: "status 상태가 declined로 변경되었습니다.",
+      data: {
+        matchData: matchData, // 업데이트된 문서 수 반환
+      },
+    });
+    } catch (error) {
+      // 에러 처리
+      console.error("status 상태 변경 오류:", error);
+      return res.status(500).json({
+        status: "error",
+        message: "status 상태 변경 중 오류가 발생했습니다.",
+        error: error.message,
+      });
+    }
 });
 
 
